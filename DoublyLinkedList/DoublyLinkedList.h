5fca595b42323862fe12402222fadec945aafbdc
Andy <40427428+anschlei@users.noreply.github.com>
2023-03-08 15:09:28 +0100

    updated github formations for DoublyLinkedList.h

--- DoublyLinkedList/DoublyLinkedList.h
+++ DoublyLinkedList/DoublyLinkedList.h
@@ -9,21 +9,21 @@
 #pragma once
 
 //----------------------------------------------------------------------------------
-//									*Classes*
+//					*Classes*
 //----------------------------------------------------------------------------------
 
 namespace
 {
 	/**
 	* Template SNode
-	* @property T*		m_pData	: included  a pointer to the saved type/class
+	* @property T*	    m_pData	: included  a pointer to the saved type/class
 	* @property SNode*  m_pPre	: previous pointer to the SNode bevor
 	* @property SNode*  m_pSuc	: successor pointer to the SNode after
 	*/
 	template <typename T>
 	struct SNode
 	{
-		T*		m_pData = nullptr;
+		T*	m_pData = nullptr;
 		SNode*  m_pPre  = nullptr;
 		SNode*  m_pSuc  = nullptr;
 	};
@@ -35,7 +35,7 @@ namespace List
 	* Template CDoublyLinkedList
 	* @property NodePtr m_pStartNode	: hold the first element or nullptr
 	* @property NodePtr m_pEndNode		: hold the last element or nullptr
-	* @property size_t m_Size			: hold the current size of the list
+	* @property size_t  m_Size		: hold the current size of the list
 	*/
 	template <typename T>
 	class CDoublyLinkedList
@@ -43,7 +43,7 @@ namespace List
 		using X			= T;
 		using XPtr		= X*;
 		using Node		= SNode<X>;
-		using NodePtr	= Node*;
+		using NodePtr		= Node*;
 
 	public:
 		CDoublyLinkedList	();
@@ -53,9 +53,9 @@ namespace List
 		void PushBack	(XPtr _pNewData);
 		void PushFront	(XPtr _pNewData);
 
-		bool InsertAtIndex		(size_t _Index, XPtr _pNewData);
-		XPtr At					(size_t _Index);
-		bool RemoveAt			(size_t _Index);
+		bool InsertAtIndex	(size_t _Index, XPtr _pNewData);
+		XPtr At			(size_t _Index);
+		bool RemoveAt		(size_t _Index);
 		size_t Size();
 
 		XPtr PopBack  ();
@@ -73,7 +73,7 @@ namespace List
 
 
 //----------------------------------------------------------------------------------
-//							*Template Class Functions*
+//				*Template Class Functions*
 //----------------------------------------------------------------------------------
 
 namespace List
@@ -85,7 +85,7 @@ namespace List
 	CDoublyLinkedList<T>::CDoublyLinkedList()
 		: m_pStartNode	(nullptr)
 		, m_pEndNode	(nullptr)
-		, m_Size		(0)
+		, m_Size	(0)
 	{}
 
 	/**
@@ -106,9 +106,9 @@ namespace List
 			{
 				delete m_pEndNode;
 
-				m_pStartNode = nullptr;
+				m_pStartNode 	= nullptr;
 
-				m_pEndNode	 = nullptr;
+				m_pEndNode	= nullptr;
 			}
 
 			--m_Size;
@@ -122,19 +122,19 @@ namespace List
 	template <typename T>
 	void CDoublyLinkedList<T>::PushBack(XPtr _pNewData)
 	{
-		NodePtr pNewNode = new Node;
+		NodePtr pNewNode  = new Node;
 
 		pNewNode->m_pData = _pNewData;
 
 		if (m_Size > 0)
 		{
-			m_pEndNode->m_pSuc = pNewNode;
+			m_pEndNode->m_pSuc 	= pNewNode;
 
-			pNewNode->m_pPre = m_pEndNode;
+			pNewNode->m_pPre   	= m_pEndNode;
 
-			pNewNode->m_pSuc = nullptr;
+			pNewNode->m_pSuc   	= nullptr;
 
-			m_pEndNode		 = pNewNode;
+			m_pEndNode		= pNewNode;
 		}
 		else // first element
 		{
@@ -142,7 +142,7 @@ namespace List
 
 			pNewNode->m_pSuc	 = m_pEndNode;
 
-			m_pEndNode			 = pNewNode;
+			m_pEndNode		 = pNewNode;
 								 
 			m_pStartNode		 = pNewNode;
 								 
@@ -159,33 +159,33 @@ namespace List
 	template<typename T>
 	void CDoublyLinkedList<T>::PushFront(XPtr _pNewData)
 	{
-		NodePtr pNewNode = new Node;
+		NodePtr pNewNode  = new Node;
 
 		pNewNode->m_pData = _pNewData;
 
 		if (m_Size > 0)
 		{
-			pNewNode->m_pPre			 = m_pStartNode->m_pPre;
+			pNewNode->m_pPre		= m_pStartNode->m_pPre;
 
-			pNewNode->m_pSuc			 = m_pStartNode;
+			pNewNode->m_pSuc		= m_pStartNode;
 			
-			m_pStartNode				 = pNewNode;
+			m_pStartNode			= pNewNode;
 			
-			m_pStartNode->m_pSuc->m_pPre = pNewNode;
+			m_pStartNode->m_pSuc->m_pPre 	= pNewNode;
 		}
 		else // first element
 		{
-			pNewNode->m_pPre	 = m_pStartNode;
+			pNewNode->m_pPre	= m_pStartNode;
 
-			pNewNode->m_pSuc	 = m_pEndNode;
+			pNewNode->m_pSuc	= m_pEndNode;
 
-			m_pEndNode			 = pNewNode;
+			m_pEndNode		= pNewNode;
 
-			m_pStartNode		 = pNewNode;
+			m_pStartNode		= pNewNode;
 
-			m_pEndNode->m_pSuc	 = nullptr;
+			m_pEndNode->m_pSuc	= nullptr;
 
-			m_pStartNode->m_pPre = nullptr;
+			m_pStartNode->m_pPre 	= nullptr;
 		}
 
 		++m_Size;
@@ -195,15 +195,15 @@ namespace List
 	* Insert a new element between the index pre and suc SNode*.
 	* @property size_t _Index
 	* @property XPtr _pNewData	: a pointer from the storage object T
-	* @return bool				: if insert at index in range of the list size
+	* @return bool			: if insert at index in range of the list size
 	*/
 	template<typename T>
 	bool CDoublyLinkedList<T>::InsertAtIndex(size_t _Index, XPtr _pNewData)
 	{
-		NodePtr pSearchNode = m_pStartNode;
+		NodePtr pSearchNode 	= m_pStartNode;
 		NodePtr pNewNode	= new Node;
 
-		pNewNode->m_pData = _pNewData;
+		pNewNode->m_pData 	= _pNewData;
 
 		if (_Index > m_Size) return false;
 
@@ -215,13 +215,13 @@ namespace List
 				pSearchNode = pSearchNode->m_pSuc;
 			}
 
-			pNewNode->m_pPre = pSearchNode;
+			pNewNode->m_pPre 		= pSearchNode;
 
-			pNewNode->m_pSuc = pSearchNode->m_pSuc;
+			pNewNode->m_pSuc 		= pSearchNode->m_pSuc;
 
-			pSearchNode->m_pSuc->m_pPre = pNewNode;
+			pSearchNode->m_pSuc->m_pPre 	= pNewNode;
 
-			pSearchNode->m_pSuc = pNewNode;
+			pSearchNode->m_pSuc 		= pNewNode;
 
 			++m_Size;
 		}
@@ -233,31 +233,31 @@ namespace List
 
 			m_pStartNode		= pNewNode;
 			
-			m_pEndNode			= pNewNode;
+			m_pEndNode		= pNewNode;
 			
 			++m_Size;
 		}
 		else if (_Index == 0) // insert at the begin
 		{
-			pNewNode->m_pSuc		= m_pStartNode;
+			pNewNode->m_pSuc	= m_pStartNode;
 
-			pNewNode->m_pPre		= nullptr;
+			pNewNode->m_pPre	= nullptr;
 
 			m_pStartNode->m_pPre	= pNewNode;
 
-			m_pStartNode			= pNewNode;
+			m_pStartNode		= pNewNode;
 
 			++m_Size;
 		}
 		else if (_Index+1 == m_Size) // insert at the end
 		{
-			pNewNode->m_pPre			= m_pEndNode;
+			pNewNode->m_pPre		= m_pEndNode;
 
 			m_pEndNode->m_pPre->m_pSuc	= pNewNode;
 
-			m_pEndNode					= pNewNode;
+			m_pEndNode			= pNewNode;
 
-			pNewNode->m_pSuc			= nullptr;
+			pNewNode->m_pSuc		= nullptr;
 
 			++m_Size;
 		}
@@ -268,7 +268,7 @@ namespace List
 	/**
 	* Gets the element from the index.
 	* @property size_t _Index	: gest the SNode-Data from the index
-	* @return T* SNode->m_pData : holds a pointer to the storage object
+	* @return T* SNode->m_pData 	: holds a pointer to the storage object
 	*/
 	template<typename T>
 	T* CDoublyLinkedList<T>::At(size_t _Index)
@@ -289,7 +289,7 @@ namespace List
 	/**
 	* Remove the element at the index.
 	* @property size_t _Index	: holds the target index
-	* @return bool				: if the element exist and was deleted returns true
+	* @return bool			: if the element exist and was deleted returns true
 	*/
 	template<typename T>
 	bool CDoublyLinkedList<T>::RemoveAt(size_t _Index)
@@ -315,9 +315,9 @@ namespace List
 		}
 		else if (m_Size == 1) // last element
 		{
-			m_pStartNode = nullptr;
+			m_pStartNode 	= nullptr;
 
-			m_pEndNode = nullptr;
+			m_pEndNode 	= nullptr;
 		}
 		else // other elements
 		{
@@ -349,7 +349,7 @@ namespace List
 		{
 			T* pReturnValue		= m_pEndNode->m_pData;
 			
-			m_pEndNode			= m_pEndNode->m_pPre;
+			m_pEndNode		= m_pEndNode->m_pPre;
 
 			delete m_pEndNode->m_pSuc;
 
@@ -363,7 +363,7 @@ namespace List
 		{
 			T* pReturnValue = m_pEndNode->m_pData;
 
-			m_pEndNode = m_pEndNode->m_pPre;
+			m_pEndNode 	= m_pEndNode->m_pPre;
 
 			delete m_pEndNode;
 
@@ -404,7 +404,7 @@ namespace List
 		{
 			T* pReturnValue = m_pStartNode->m_pData;
 
-			m_pStartNode = m_pStartNode->m_pSuc;
+			m_pStartNode 	= m_pStartNode->m_pSuc;
 
 			delete m_pStartNode;
 
@@ -423,7 +423,7 @@ namespace List
 	/**
 	* Operator overloading [], with the same function as At function.
 	* @property size_t _Index	: hold the target index
-	+ @return T* SNode->m_pData : holds a pointer to the storage object
+	* @return T* SNode->m_pData 	: holds a pointer to the storage object
 	*/
 	template<typename T>
 	T* CDoublyLinkedList<T>::operator[](size_t _Index)
